49190247109a29c644dabb8365b41b2757999d45
Max <tutv95@gmail.com>
2018-02-11 19:11:49 +0700

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import 'antd/dist/antd.css';
+import "bootstrap/dist/css/bootstrap.css";
 import Recorder from "./Recorder";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="App ">
                 <Recorder/>
             </div>
         );
--- src/Recorder.js
+++ src/Recorder.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import {ReactMic} from 'react-mic';
-import {Button} from "antd";
 
 class Recorder extends Component {
     constructor(props) {
@@ -34,20 +33,36 @@ class Recorder extends Component {
         const {audio} = this.state;
 
         return (
-            <div>
-                <ReactMic
-                    record={this.state.record}
-                    className="sound-wave"
-                    onStop={this.onStop}
-                    strokeColor="#000000"
-                    backgroundColor="#FF4081"/>
-                <Button onClick={this.startRecording}>Start</Button>
-                <Button onClick={this.stopRecording}>Stop</Button>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <h2>Demo recorder</h2>
 
-                {
-                    !!audio &&
-                    <audio controls src={audio}/>
-                }
+                        <ReactMic
+                            record={this.state.record}
+                            className="sound-wave"
+                            onStop={this.onStop}
+                            strokeColor="#000000"
+                            backgroundColor="#FF4081"/>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center">
+                    <div className="col-6" style={{marginBottom: '20px'}}>
+                        <button className="btn btn-primary" type="primary" onClick={this.startRecording}>Start</button>
+                        {' '}
+                        <button className="btn btn-danger" onClick={this.stopRecording}>Stop</button>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        {
+                            !!audio &&
+                            <audio controls src={audio}/>
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
