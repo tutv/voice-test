0fd7ff922ae66193ab5a5c24b6c26656b85ec36d
Max <tutv95@gmail.com>
2018-02-26 23:56:29 +0700

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import MediaRecorder from 'audio-recorder-polyfill';
-window.MediaRecorder = MediaRecorder;
-
 ReactDOM.render(<App/>, document.getElementById('root'));
